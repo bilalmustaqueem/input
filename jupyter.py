1b2ec392686d75ec5a32533d2cde27cc65fcc1b0
bilalmustaqueem <bilalmustaqueem438@gmail.com>
2025-05-30 20:01:22 +0500

    input code

---
+++ jupyter.py
@@ -0,0 +1,19 @@
+z = []
+for s in range(3):
+    a = input()
+    z.append(a)
+
+print(z)
+
+z[0]
+f = "hello"
+print(f + ' ' + z[0])
+
+z[1]
+print(f + " " + z[1])
+
+z[2]
+print(f + " " + z[2])
+
+for v in range(3):
+    print(f + " " + z[v])
